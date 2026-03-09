e27aa536b7bffc446e6e2f6bee442b5de3bd82d6
Tim Nichols <timnichols@Tims-MacBook-Pro.local>
2026-03-09 14:45:49 -0500

    Replace VertalisWord with build-safe client component

--- components/VertalisWord.tsx
+++ components/VertalisWord.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 
 type VertalisWordProps = {
@@ -7,24 +9,15 @@ type VertalisWordProps = {
   children?: React.ReactNode;
 };
 
-const baseStyle: React.CSSProperties = {
-  fontFamily: 'ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
-  color: "#c06020",
-  textShadow:
-    "0 1px 0 rgba(255,255,255,0.22), 0 14px 30px rgba(192,96,32,0.38), 0 26px 58px rgba(0,0,0,0.55)",
-};
-
 export default function VertalisWord({
+  as: Component = "div",
   className,
   style,
-  as = "div",
-  children,
+  children = "Vertalis",
 }: VertalisWordProps) {
-  const Component = as || "div";
-
   return (
-    <Component className={className} style={{ ...baseStyle, ...style }}>
-      {children ?? "Vertalis"}
+    <Component className={className} style={style}>
+      {children}
     </Component>
   );
 }
