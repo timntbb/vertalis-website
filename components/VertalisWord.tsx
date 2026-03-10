e8b34edbf7736ce845a580deaf04422dc522d69f
Tim Nichols <timnichols@Tims-MacBook-Pro.local>
2026-03-09 21:37:10 -0500

    Restore VertalisWord default brand styling

--- components/VertalisWord.tsx
+++ components/VertalisWord.tsx
@@ -9,6 +9,13 @@ type VertalisWordProps = {
   children?: React.ReactNode;
 };
 
+const baseStyle: React.CSSProperties = {
+  fontFamily: 'ui-serif, Georgia, Cambria, "Times New Roman", Times, serif',
+  color: "#c06020",
+  textShadow:
+    "0 1px 0 rgba(255,255,255,0.22), 0 14px 30px rgba(192,96,32,0.38), 0 26px 58px rgba(0,0,0,0.55)",
+};
+
 export default function VertalisWord({
   as: Component = "div",
   className,
@@ -16,7 +23,7 @@ export default function VertalisWord({
   children = "Vertalis",
 }: VertalisWordProps) {
   return (
-    <Component className={className} style={style}>
+    <Component className={className} style={{ ...baseStyle, ...style }}>
       {children}
     </Component>
   );
