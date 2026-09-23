87b4e9b6100212cc14c0c5839144c105fb5c2ef2
timntbb <timntbb@gmail.com>
2026-09-23 16:23:17 -0500

    Move desktop consultation CTA 50px farther right

--- app/page.tsx
+++ app/page.tsx
@@ -554,7 +554,7 @@ export function Header() {
             <CapabilitiesNavItem />
           </nav>
 
-          <div className="relative left-7 hidden shrink-0 lg:block xl:left-10">
+          <div className="relative left-[78px] hidden shrink-0 lg:block xl:left-[90px]">
             <Button
               href="/consultation"
               className="rounded-full px-6 py-2 text-sm tracking-[0.12em]"
